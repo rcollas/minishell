5984326f1a89b168b80542b4b007261574c7f4ef
Robin COLLAS <rcollas@e2r8p21.clusters.42paris.fr>
2021-09-30 18:49:05 +0200

    depart sur les chapeaux de roue

---
+++ Makefile
@@ -0,0 +1,43 @@
+NAME		=	minishell
+
+SRC_FILES	=	src/main.c \
+
+LIBFT_PATH	=	libft/
+
+LIBFT_LIB	=	libft/libft.a
+
+INCLUDE_PATH	=	include/
+
+SRC_OBJS	=	$(SRC_FILES:.c=.o)
+
+LIBFTMAKE	=	$(MAKE) --no-print-directory -C $(LIBFT_PATH)
+
+CC		=	clang
+
+CFLAGS		=	-Wall -Wextra -Werror -I$(INCLUDE_PATH) 
+
+RM		=	rm -rf
+
+%.o : %.c
+	@$(CC) -c $(CFLAGS) $< -o $@
+
+all:			$(NAME)
+
+$(NAME):		$(SRC_OBJS)
+				@$(LIBFTMAKE)
+				@$(CC) -lreadline $(CFLAGS) $(SRC_OBJS) $(LIBFT_LIB) -o $(NAME)
+				@echo "$(NAME) created"
+
+clean:
+			@make --no-print-directory -C $(LIBFT_PATH) clean
+			@$(RM) $(SRC_OBJS)
+			@echo "$(NAME) .o deleted"
+
+fclean:			clean
+				@$(RM) $(NAME) $(LIBFT_LIB)
+				@echo "libft.a deleted"
+				@echo "$(NAME) deleted"
+
+re:			fclean all
+
+.PHONY:			all clean fclean re
---
+++ include/minishell.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcollas <rcollas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 17:23:47 by rcollas           #+#    #+#             */
+/*   Updated: 2021/09/30 18:08:36 by rcollas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_var{
+	char	**env;
+}		t_var;
+
+typedef struct s_builtin{
+	char	*cmd;
+	int	(*func)(t_var);
+}		t_builtin;
+
+#endif
