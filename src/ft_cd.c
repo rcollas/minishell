1b549aaee46e3ae3d2d1897cd0e3b180cbad978c
Vincent BACHELET <vbachele@e3r6p16.42.fr>
2021-10-07 11:56:31 +0200

    v2 cd

--- src/ft_cd.c
+++ src/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 16:43:47 by vbachele          #+#    #+#             */
-/*   Updated: 2021/10/06 18:04:26 by vbachele         ###   ########.fr       */
+/*   Updated: 2021/10/07 11:34:58 by vbachele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,20 +38,45 @@ int ft_cd(t_var *var)
 	int 	dir;
 	t_list 	*tmp;
 	t_envar	*tmp2;
+	t_envar	*tmp3;
+	t_envar	*tmp4;
+	char	*str;
 	
 	var->cmd = &(var->cmd[3]);
 	print_echo(var);
 	tmp = var->list;
 	tmp2 = var->envar;
 	dir = chdir(tmp->content);
+	tmp3 = var->envar;
+	tmp4 = var->envar;
 	// if (tmp->content == 0)
 	// {
 	// 	dir = chdir("~");
 	// 	printf("%s\n", getcwd(0, 150));
 	// 	return(0);
 	// }
+	printf("%s\n", getcwd(0, 150));
+	while(tmp3)
+	{
+		if(ft_strcmp(tmp3->name, "OLDPWD"))
+		{
+			str = tmp3->content;
+			break;
+		}
+		tmp3 = tmp3->next;
+	}
+	while(tmp4)
+	{
+		if (ft_strcmp(tmp4->name, "PWD"))
+		{
+			break;
+		}
+		tmp4 = tmp4->next;
+	}
+	tmp4->content = tmp3->content;
+	tmp3->content = str;
 	if (dir < 0)
 		Errors_chdir_handling(dir);
-	printf("%s\n", getcwd(0, 150));
+	// printf("%s\n", getcwd(0, 150));
 	return (0);
 }
